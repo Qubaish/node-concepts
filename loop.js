12237ac0c67300b6140cc36db877f5b16843887c
qubaish <qbhatti143@gmail.com>
2020-06-27 17:43:54 +0400

    threads

---
+++ loop.js
@@ -0,0 +1,13 @@
+const pendingTimes = [];
+const pendingOsTasks = [];
+const pendingOperations = [];
+
+function shouldContinue(){
+    // check pending timers, (setimeout)
+    // check pending OS tasks ? (server listening)
+    // check pending long running operatiosn (fs module)
+};
+
+while(shouldContinue()){
+
+};
